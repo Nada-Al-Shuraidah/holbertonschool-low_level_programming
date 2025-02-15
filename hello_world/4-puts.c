27f607df1395687beef687e00c3c363e20c32722
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-02-15 06:15:10 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,8 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line.
+ * main - prints "Programming is like building a multilingual 
+ * puzzle, followed by a new line.
  * Return: Always 0.
  */
 
@@ -10,6 +11,5 @@ int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 } 

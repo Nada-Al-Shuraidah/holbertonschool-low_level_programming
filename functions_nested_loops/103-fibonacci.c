482e369090b756dae0f7254b5ec6120d8d8e46f1
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-02-19 15:35:26 +0000

    U

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+unsigned long fib1 = 1, fib2 = 2, next, sum = 0;
+
+while (fib2 <= 4000000)
+{
+if (fib2 % 2 == 0)
+{
+sum += fib2;
+}
+
+next = fib1 + fib2;
+fib1 = fib2;
+fib2 = next;
+}
+
+printf("%lu\n", sum);
+return (0);
+}

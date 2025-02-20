7c2f170d73358f5476c23ee09db246651ee152d7
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-02-20 14:52:49 +0000

    Update

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -8,17 +8,43 @@
 */
 int main(void)
 {
-unsigned long a = 1, b = 2, next;
-int count;
+unsigned long int i;
+unsigned long int a1 = 1, a2 = 2, b1;
+unsigned long int a1_high, a1_low, a2_high, a2_low;
+unsigned long int high, low;
 
-printf("%lu, %lu", a, b);
+printf("%lu, %lu", a1, a2);
 
-for (count = 2; count < 98; count++)
+for (i = 2; i < 98; i++)
 {
-next = a + b;
-printf(", %lu", next);
-a = b;
-b = next;
+if (i < 91)
+{
+b1 = a1 + a2;
+a1 = a2;
+a2 = b1;
+printf(", %lu", b1);
+}
+else
+{
+a1_high = a1 / 1000000000;
+a1_low = a1 % 1000000000;
+a2_high = a2 / 1000000000;
+a2_low = a2 % 1000000000;
+
+high = a1_high + a2_high;
+low = a1_low + a2_low;
+
+if (low > 999999999)
+{
+high += 1;
+low %= 1000000000;
+}
+
+printf(", %lu%09lu", high, low);
+
+a1 = a2;
+a2 = high * 1000000000 + low;
+}
 }
 printf("\n");
 return (0);

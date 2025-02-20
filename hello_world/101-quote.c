3be43f3727438e0310e1cc4e61bcde17460d3910
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-02-20 16:29:34 +0000

    U

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Prints a quote to standard error.
+ *
+ * Return: 1 (Error).
+ */
+int main(void)
+{
+char quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+write(2, quote, sizeof(quote) - 1); // Write to stderr (file descriptor 2)
+return (1);
+}

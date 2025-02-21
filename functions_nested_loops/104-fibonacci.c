47f29a881ba0daf41b4119727a5bda77d8ed70bc
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-02-21 08:18:24 +0000

    Update

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -2,34 +2,13 @@
 #include <stdio.h>
 
 /**
- * print_first_fibonacci - Prints the first 92 Fibonacci numbers.
- */
-void print_first_fibonacci(void)
-{
-unsigned long a = 1, b = 2, temp;
-int count;
-
-printf("%lu, %lu", a, b);
-
-for (count = 3; count <= 92; count++)
-{
-temp = a + b;
-printf(", %lu", temp);
-a = b;
-b = temp;
-}
-printf(", ");
-}
-
-/**
- * print_large_fibonacci-Prints Fibonacci numbers beyond 92 using split digits.
+ * print_large_fibonacci - Print large Fibonacci numbers correctly
  * @a_high: High part of first number
  * @a_low: Low part of first number
  * @b_high: High part of second number
  * @b_low: Low part of second number
  */
-void print_large_fibonacci(
-unsigned long a_high, unsigned long a_low,
+void print_large_fibonacci(unsigned long a_high, unsigned long a_low,
 unsigned long b_high, unsigned long b_low)
 {
 unsigned long temp_high, temp_low;
@@ -44,31 +23,36 @@ if (temp_low >= 1000000000)
 {
 temp_high += 1;
 temp_low -= 1000000000;
-}
 
-printf("%lu%09lu", temp_high, temp_low);
-if (count != 98)
-printf(", ");
+}
+printf(", %lu%09lu", temp_high, temp_low);
 
 a_high = b_high;
 a_low = b_low;
 b_high = temp_high;
 b_low = temp_low;
-
 }
-printf("\n");
 }
-
 /**
- * main - Entry point
- * Return: Always 0.
+ * main - Prints the first 98 Fibonacci numbers, starting with 1 and 2 .
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
+unsigned long a = 1, b = 2, temp;
 unsigned long a_high, a_low, b_high, b_low;
-unsigned long a = 1, b = 2;
+int count;
 
-print_first_fibonacci();
+printf("%lu, %lu", a, b);
+
+for (count = 3; count <= 92; count++)
+{
+temp = a + b;
+printf(", %lu", temp);
+a = b;
+b = temp;
+}
 
 a_high = a / 1000000000;
 a_low = a % 1000000000;
@@ -76,6 +60,6 @@ b_high = b / 1000000000;
 b_low = b % 1000000000;
 
 print_large_fibonacci(a_high, a_low, b_high, b_low);
-
+printf("\n");
 return (0);
 }

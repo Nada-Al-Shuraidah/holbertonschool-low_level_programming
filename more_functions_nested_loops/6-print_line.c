ae0d1ce66aaffde2e7a986d8aed84788420fa2b5
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-02-24 15:46:24 +0000

    6-print_line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,22 +2,15 @@
 
 /**
 * print_line - Draws a straight line in the terminal.
-* @n: The number of times the character '_' should be printed.
+* @n: The number of times the character _ should be printed.
 */
 void print_line(int n)
 {
 int i;
 
-if (n <= 0)
-{
-_putchar('\n');
-}
-else
-{
 for (i = 0; i < n; i++)
 {
-putchar('');
+_putchar('_');
 }
 _putchar('\n');
-}
 }

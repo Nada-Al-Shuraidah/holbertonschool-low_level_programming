564c98d543006d4fabef55d0781fec1278f98a24
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-02-27 14:30:35 +0000

    Update

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,7 +16,7 @@ int _atoi(char *s);
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
-int _strcmo(char *s1, char *s2);
+int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);

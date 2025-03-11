caa0e8dd232582058f955b5ff4336a0c99e158b5
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-03-11 20:39:51 +0000

    Update

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 
 void *malloc_checked(unsigned int b);
-int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 int _putchar(char c);
 
 #endif /* MAIN_H */

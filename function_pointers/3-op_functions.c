d9ab470c5b6356ebce48136559c1ce53a144341e
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-03-16 19:01:45 +0000

    Update

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Sum of a and b
+ */
+int op_add(int a, int b)
+{
+    return (a + b);
+}
+
+/**
+ * op_sub - Subtracts two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Difference of a and b
+ */
+int op_sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Product of a and b
+ */
+int op_mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+ * op_div - Divides two numbers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+    if (b == 0)
+    {
+        return (0); /* Division by zero is handled in main */
+    }
+    return (a / b);
+}
+
+/**
+ * op_mod - Computes the remainder of the division
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: Remainder of a divided by b
+ */
+int op_mod(int a, int b)
+{
+    if (b == 0)
+    {
+        return (0); /* Modulo by zero is handled in main */
+    }
+    return (a % b);
+}

8edd7a20a4f64a9e26c194a0fc12693cba4dc867
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-04-01 15:59:27 +0000

    Update

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -4,18 +4,18 @@
 #include <stddef.h> /* Include for size_t type */
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: This struct represents a node in a singly linked list
- */
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: This struct represents a node in a singly linked list
+*/
 typedef struct list_s
 {
-    char *str;              /* Pointer to a string */
-    unsigned int len;       /* Length of the string */
-    struct list_s *next;    /* Pointer to the next node */
+char *str;              /* Pointer to a string */
+unsigned int len;       /* Length of the string */
+struct list_s *next;    /* Pointer to the next node */
 } list_t;
 
 /* Prototype for the print_list function */
